a1d6999cd3ab70f338427743b76bba8c0c57ea01
abdusamadweb <abdusamadweb@gmail.com>
2023-09-02 20:24:41 +0500

    access for facebook

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Hero from "./hero/Hero";
 import Direction from "./direction/Direction";
+import News from "../alidoorf-page/main/news/News";
+import Instagram from "../alidoorf-page/about/instagram/Instagram";
+import Contact from "../alidoorf-page/main/contact/Contact";
 
 const Home = ({ lang }) => {
 
@@ -8,6 +11,9 @@ const Home = ({ lang }) => {
         <main>
             <Hero lang={lang} />
             <Direction lang={lang} />
+            <News lang={lang} />
+            <Instagram lang={lang} />
+            <Contact lang={lang} />
         </main>
     );
 };
